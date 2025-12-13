0bbb8b818644d9c4296f21e80544c100932c7daf
interlektyr <interlektyr@hotmail.com>
2025-12-13 23:01:37 +0100

    Add files via upload

--- usrlocalbin/bspwm_workspaces.sh
+++ usrlocalbin/bspwm_workspaces.sh
@@ -1,53 +1,67 @@
 #!/bin/bash
 
-next_wc () {
-#bspc desktop -f next.local
-bspc desktop -f next.local
-set_wc
+next_wc() {
+  #bspc desktop -f next.local
+  bspc desktop -f next.local
+  set_wc
 }
 
-prev_wc () {
-bspc desktop -f prev.local
-set_wc 
+prev_wc() {
+  bspc desktop -f prev.local
+  set_wc
 }
 
-jump_wc () {
-bspc desktop --focus "^${selwc}"
-set_wc
+jump_wc() {
+  bspc desktop --focus "^${selwc}"
+  set_wc
 }
 
-set_wc () {
-xrandr -q | grep "DP-1-1 connected" && cw=$(bspc query -d 'DP-1-1:focused' -D --names)
-xrandr -q | grep "DP-1-1 connected" || cw=$(bspc query -D -d focused --names)
-sel=$(sed -n "${cw}p" < /tmp/randwall.txt)
-hsetroot -cover $sel
-#dunstify -h string:x-dunst-stack-tag:wc "Desktop $cw" -i 1
+set_wc() {
+
+  if [ -f /tmp/external_m.txt ]; then
+    cw=$(bspc query -d 'DP-1-1:focused' -D --names)
+  else
+    cw=$(bspc query -D -d focused --names)
+  fi
+
+  #xrandr -q | grep "DP-1-1 connected" && cw=$(bspc query -d 'DP-1-1:focused' -D --names)
+  #xrandr -q | grep "DP-1-1 connected" || cw=$(bspc query -D -d focused --names)
+
+  sel=$(sed -n "${cw}p" </tmp/randwall.txt)
+  hsetroot -cover $sel
+  #dunstify -h string:x-dunst-stack-tag:wc "Desktop $cw" -i 1
 }
 
-send_wc () {
-bspc node --to-desktop "^${sendwc}"
-dunstify -i /$HOME/.config/bspwm/bspwm_logo.svg -h string:x-dunst-stack-tag:wc "Window was sent to Desktop $sendwc"
+send_wc() {
+  bspc node --to-desktop "^${sendwc}"
+  dunstify -i /$HOME/.config/bspwm/bspwm_logo.svg -h string:x-dunst-stack-tag:wc "Window was sent to Desktop $sendwc"
 }
 
-send_wc_follow () {
-bspc node --to-desktop "^${sendwcf}" --follow
-set_wc
+send_wc_follow() {
+  bspc node --to-desktop "^${sendwcf}" --follow
+  set_wc
 }
 
 case $1 in
-    next)
-    next_wc;;
-    prev)
-    prev_wc;;
-    jump)
-    selwc=$2
-    jump_wc;;
-    send)
-    sendwc=$2
-    send_wc;;
-    sendf)
-    sendwcf=$2
-    send_wc_follow;;
-    *)
-    exit;;
+next)
+  next_wc
+  ;;
+prev)
+  prev_wc
+  ;;
+jump)
+  selwc=$2
+  jump_wc
+  ;;
+send)
+  sendwc=$2
+  send_wc
+  ;;
+sendf)
+  sendwcf=$2
+  send_wc_follow
+  ;;
+*)
+  exit
+  ;;
 esac
