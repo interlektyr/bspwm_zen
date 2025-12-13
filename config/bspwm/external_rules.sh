6ed7be71aee84436f85447c10d368f2cad66cd8e
interlektyr <interlektyr@hotmail.com>
2025-12-13 20:28:46 +0100

    Add files via upload

--- config/bspwm/external_rules.sh
+++ config/bspwm/external_rules.sh
@@ -4,45 +4,36 @@
 if [ -f /tmp/external_m.txt ]; then
 
   tw=$(bspc query -d 'DP-1-1:focused' -D --names)
-
-  if [ "$(sed -n "${tw}p" </tmp/external_m.txt)" = "tile" ]; then
-
-    setl="tiled"
-    comid=$(xprop -notype -id $1 WM_COMMAND)
-
-    for exp in $(cat /$HOME/.config/bspwm/bspwm_exceptions.txt); do
-
-      #if [ "$comid" == *"$exp"* ]; then
-      if [ -n "$(xprop -notype -id $1 WM_COMMAND | grep "$exp")" ]; then
-        setl="floating"
-      fi
-
-    done
-
-    if [ "$setl" = "tiled" ]; then
-
-      echo 'state=tiled'
-
-    else
-
-      echo 'state=floating'
-
-    fi
-
-  fi
-
-  #if [ -z "$(xprop -notype -id $1 WM_COMMAND | grep "appcommander.sh")" ]; then
-
-  #  echo 'state=tiled'
-
-  #fi
+  locfi="/tmp/external_m.txt"
 
 else
 
   tw=$(bspc query -D -d focused --names)
+  locfi="/tmp/internal_m.txt"
 
-  if [ "$(sed -n "${tw}p" </tmp/internal_m.txt)" = "tile" ]; then
+fi
+
+if [ "$(sed -n "${tw}p" <"$locfi")" = "tile" ]; then
+
+  setl="tiled"
+  comid=$(xprop -notype -id $1 WM_COMMAND)
+
+  for exp in $(cat /$HOME/.config/bspwm/bspwm_exceptions.txt); do
+
+    if [ -n "$(xprop -notype -id $1 WM_COMMAND | grep "$exp")" ]; then
+      setl="floating"
+    fi
+
+  done
+
+  if [ "$setl" = "tiled" ]; then
+
     echo 'state=tiled'
+
+  else
+
+    echo 'state=floating'
+
   fi
 
 fi
