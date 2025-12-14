adb12294349d67c92cdeb534781aaa8d835dfd68
interlektyr <interlektyr@hotmail.com>
2025-12-14 11:28:42 +0100

    Add files via upload

--- bspwm_appinstaller.sh
+++ bspwm_appinstaller.sh
@@ -6,14 +6,14 @@ if [ -f /$HOME/.config/bspwm/packages.gz ]; then
   rm /$HOME/.config/bspwm/packages.gz
 fi
 
-gum spin --spinner pulse --title "Syncing databases" -- wget https://aur.archlinux.org/packages.gz -P /$HOME/.config/bspwm/ >/dev/null
+gum spin --spinner pulse --title "Syncing databases" -- wget -q https://aur.archlinux.org/packages.gz -P /$HOME/.config/bspwm/ >/dev/null
 
 for aurapps in $(zcat /$HOME/.config/bspwm/packages.gz); do
   echo "AUR $aurapps" >>/$HOME/.config/bspwm/bspwm_applist.txt
 done
 
 #va=$(gum filter --no-limit --header "AppInstaller" </$HOME/.config/bspwm/bspwm_applist.txt)
-va=$(cat /$HOME/.config/bspwm/bspwm_applist.txt | fzf --border --border-label=" AppInstaller " --color=label:italic:yellow)
+va=$(cat /$HOME/.config/bspwm/bspwm_applist.txt | fzf --border --border-label=" AppInstaller " --color=label:italic:yellow --color=border:'#1D2021')
 
 final=$(echo $va | awk '{print $2}')
 
