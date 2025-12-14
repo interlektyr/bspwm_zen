4b314b763476398772cb929830b6650074fa7075
interlektyr <interlektyr@hotmail.com>
2025-12-14 20:41:16 +0100

    Add files via upload

--- usrlocalbin/appcommander.sh
+++ usrlocalbin/appcommander.sh
@@ -1,59 +1,71 @@
 #!/bin/bash
 
-truncate -s 0 /tmp/appcommander/AC_Exec.txt
-truncate -s 0 /tmp/appcommander/AC_Name.txt
-truncate -s 0 /tmp/appcommander/AC_Term.txt
-truncate -s 0 ~/nohup.out
+update() {
 
-echo "AppCommander custum launcher script (close)" >>/tmp/appcommander/AC_Name.txt
-echo "null" >>/tmp/appcommander/AC_Exec.txt
-echo "exit" >>/tmp/appcommander/AC_Term.txt
+  truncate -s 0 /tmp/appcommander/AC_Exec.txt
+  truncate -s 0 /tmp/appcommander/AC_Name.txt
+  truncate -s 0 /tmp/appcommander/AC_Term.txt
 
-for entry in $(find /usr/share/applications -name "*.desktop"); do
-  onlyname=$(echo $entry | cut -d/ -f5)
-  echo "$entry" >>/tmp/appcommander/AC_Exec.txt
-  nameApp=$(grep -m 1 '^Name=' $entry | head -1 | cut -d= -f2)
+  echo "ENTER:pick ESC:quit" >>/tmp/appcommander/AC_Name.txt
+  echo "null" >>/tmp/appcommander/AC_Exec.txt
+  echo "null" >>/tmp/appcommander/AC_Term.txt
 
-  if grep -qwF "$nameApp" "/tmp/appcommander/AC_Name.txt"; then
-    echo "$nameApp ($onlyname)" >>/tmp/appcommander/AC_Name.txt
+  for entry in $(find /usr/share/applications -name "*.desktop"); do
+    onlyname=$(echo $entry | cut -d/ -f5)
+    echo "$entry" >>/tmp/appcommander/AC_Exec.txt
+    nameApp=$(grep -m 1 '^Name=' $entry | head -1 | cut -d= -f2)
+
+    if grep -qwF "$nameApp" "/tmp/appcommander/AC_Name.txt"; then
+      echo "$nameApp ($onlyname)" >>/tmp/appcommander/AC_Name.txt
+    else
+      echo "$nameApp" >>/tmp/appcommander/AC_Name.txt
+    fi
+
+    hterm=$(grep '^Terminal=' $entry | head -1 | cut -d= -f2)
+
+    if [ "$hterm" = "true" ]; then
+      echo "$(grep -m 1 '^Exec=' $entry | head -1 | cut -d= -f2)" >>/tmp/appcommander/AC_Term.txt
+    else
+      echo "false" >>/tmp/appcommander/AC_Term.txt
+    fi
+
+  done
+
+}
+
+main() {
+
+  truncate -s 0 ~/nohup.out
+
+  v=$(cat /tmp/appcommander/AC_Name.txt | fzf --layout=reverse-list --header-lines=1 --border --padding=5%,0%,0%,0% --border-label=" AppCommander custom launcher script " --color=label:italic:yellow --color=border:'#1D2021' --color=pointer:magenta --color=marker:green)
+
+  lineNum=$(grep -n "$v" "/tmp/appcommander/AC_Name.txt" | cut -d: -f1)
+
+  checkterm=$(sed -n "${lineNum}p" </tmp/appcommander/AC_Term.txt)
+
+  if [ "$checkterm" = "false" ]; then
+
+    sel=$(sed -n "${lineNum}p" </tmp/appcommander/AC_Exec.txt)
+
+    nohup dex $sel >/dev/null 2>&1
+
+    exit 0
+
   else
-    echo "$nameApp" >>/tmp/appcommander/AC_Name.txt
+
+    nohup urxvtc -g 150x50 -e sh -c "$checkterm;exit"
+
+    exit 0
+
   fi
 
-  hterm=$(grep '^Terminal=' $entry | head -1 | cut -d= -f2)
+}
 
-  if [ "$hterm" = "true" ]; then
-    echo "$(grep -m 1 '^Exec=' $entry | head -1 | cut -d= -f2)" >>/tmp/appcommander/AC_Term.txt
-  else
-    echo "false" >>/tmp/appcommander/AC_Term.txt
-  fi
-
-done
-
-v=$(cat /tmp/appcommander/AC_Name.txt | fzf --border --border-label=" AppCommander custom launcher script " --color=label:italic:yellow --color=border:'#1D2021')
-
-#v=$(gum filter --header "AppCommander custom launcher script" </tmp/appcommander/AC_Name.txt)
-
-lineNum=$(grep -n "$v" "/tmp/appcommander/AC_Name.txt" | cut -d: -f1)
-
-checkterm=$(sed -n "${lineNum}p" </tmp/appcommander/AC_Term.txt)
-
-if [ "$checkterm" = "false" ]; then
-
-  sel=$(sed -n "${lineNum}p" </tmp/appcommander/AC_Exec.txt)
-
-  nohup dex $sel >/dev/null 2>&1
-
-  exit 0
-
-else if [ "$checkterm" = "exit" ]; then
-
-  exit 0
-
-else
-
-  nohup urxvtc -g 150x50 -e sh -c "$checkterm;exit"
-
-  exit 0
-
-fi
+case $1 in
+u)
+  update
+  ;;
+*)
+  main
+  ;;
+esac
