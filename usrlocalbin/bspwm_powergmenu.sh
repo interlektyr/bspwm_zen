83b49158bed9b7e4140ed4b55125c6b0983f8d71
interlektyr <interlektyr@hotmail.com>
2025-12-16 22:41:02 +0100

    Add files via upload

--- usrlocalbin/bspwm_powergmenu.sh
+++ usrlocalbin/bspwm_powergmenu.sh
@@ -7,21 +7,21 @@ echo "Reload X11" >>/tmp/optionsP.txt
 echo "Reboot" >>/tmp/optionsP.txt
 echo "Reboot into firmware" >>/tmp/optionsP.txt
 
-case $(cat /tmp/optionsP.txt | fzf --layout=reverse-list --header-lines=1 --border --padding=5%,0%,0%,0% --border-label=" Power " --color=label:italic:yellow --color=border:'#1D2021' --color=pointer:magenta --color=marker:green) in
+case $(cat /tmp/optionsP.txt | fzf --info=hidden --header-lines=1 --color=pointer:magenta --color=border:'#1D2021' --footer="Power" --color=footer:italic:yellow) in
 "Lock")
   slock
   ;;
 "Shutdown")
-  urxvtc -g 45x5 -e sh -c "gum confirm && shutdown -h now || exit"
+  urxvtc -g 45x5 -e sh -c "gum confirm --no-show-help --prompt.foreground="#DBBC7F" --selected.background="#D699B6" && shutdown -h now || exit"
   ;;
 "Reload X11")
-  urxvtc -g 45x5 -e sh -c "gum confirm && bspc quit || exit"
+  urxvtc -g 45x5 -e sh -c "gum confirm --no-show-help --prompt.foreground="#DBBC7F" --selected.background="#D699B6" && bspc quit || exit"
   ;;
 "Reboot")
-  urxvtc -g 45x5 -e sh -c "gum confirm && systemctl reboot || exit"
+  urxvtc -g 45x5 -e sh -c "gum confirm --no-show-help --prompt.foreground="#DBBC7F" --selected.background="#D699B6" && systemctl reboot || exit"
   ;;
 "Reboot into firmware")
-  urxvtc -g 45x5 -e sh -c "gum confirm && systemctl reboot --firmware-setup || exit"
+  urxvtc -g 45x5 -e sh -c "gum confirm --no-show-help --prompt.foreground="#DBBC7F" --selected.background="#D699B6" && systemctl reboot --firmware-setup || exit"
   ;;
 *)
   exit
