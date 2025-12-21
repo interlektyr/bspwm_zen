ce5c1d90ff0458b66939927b2a16ece0ac80369f
interlektyr <interlektyr@hotmail.com>
2025-12-21 08:34:18 +0100

    Add files via upload

--- usrlocalbin/bspwm_navigator.sh
+++ usrlocalbin/bspwm_navigator.sh
@@ -1,107 +1,90 @@
 #!/bin/bash
 
-desktop() {
-
-  if [ -f /tmp/external_m.txt ]; then
-    cw=$(bspc query -d 'DP-1-1:focused' -D --names)
-  else
-    cw=$(bspc query -D -d focused --names)
-  fi
-
-  seld=$(
-
-    for wid in $(bspc query -N -n .!hidden.window -d $cw); do
-
-      n=$((n + 1))
-
-      if [ $n = 1 ]; then
-        echo "esc quit $mode"
-      else
-        echo -e "$(xprop -notype -id $wid WM_NAME | cut -d'"' -f2) | $(xprop -notype -id $wid WM_CLASS | cut -d'"' -f2)\e[1;90m | $wid\e[0m"
-      fi
-
-    done | fzf --ansi --bind "ctrl-s:execute(appcommander.sh u)+become(appcommander.sh)" \
-      --info=hidden --header-lines=1 --border --padding=5%,0%,0%,0% \
-      --border-label=" Navigator ($type) " --border-label-pos=3 --color=label:italic:yellow --color=border:'#1D2021' \
-      --color=pointer:magenta --color=marker:green --header-border=line --footer-border=line | cut -d'|' -f3
-
-  )
-
-  bspc node $seld -g hidden=off -f
-  bspc node $seld -g sticky=off -f
-
+init() {
+  touch /tmp/hwlist.txt
+  touch /tmp/hwidlist.txt
+  exit
 }
 
-hidden() {
+main() {
 
-  selh=$(
+  n=0
 
-    for wid in $(bspc query -N -n .hidden.window); do
+  if [ "$mode" = "vd" ]; then
 
-      n=$((n + 1))
+    if [ -f /tmp/external_m.txt ]; then
+      cw=$(bspc query -d 'DP-1-1:focused' -D --names)
+    else
+      cw=$(bspc query -D -d focused --names)
+    fi
 
-      if [ $n = 1 ]; then
-        echo "esc quit $mode"
-      else
-        echo -e "$(xprop -notype -id $wid WM_NAME | cut -d'"' -f2) | $(xprop -notype -id $wid WM_CLASS | cut -d'"' -f2)\e[1;90m | $wid\e[0m"
-      fi
-
-    done | fzf --ansi --bind "ctrl-s:execute(appcommander.sh u)+become(appcommander.sh)" \
-      --info=hidden --header-lines=1 --border --padding=5%,0%,0%,0% \
-      --border-label=" Navigator ($type) " --border-label-pos=3 --color=label:italic:yellow --color=border:'#1D2021' \
-      --color=pointer:magenta --color=marker:green --header-border=line --footer-border=line | cut -d'|' -f3
-
-  )
-
-  bspc node $selh -g hidden=off -f
-  bspc node $selh -g sticky=off -f
-
-}
-
-all() {
-
-  sela=$(
-
-    for wid in $(bspc query -N -n .!hidden.window); do
-
-      n=$((n + 1))
-
-      if [ $n = 1 ]; then
-        echo "esc quit $mode"
-      else
-        echo -e "$(xprop -notype -id $wid WM_NAME | cut -d'"' -f2) | $(xprop -notype -id $wid WM_CLASS | cut -d'"' -f2)\e[1;90m | $wid\e[0m"
-      fi
-
-    done | fzf --ansi --bind "ctrl-s:execute(appcommander.sh u)+become(appcommander.sh)" \
-      --info=hidden --header-lines=1 --border --padding=5%,0%,0%,0% \
-      --border-label=" Navigator ($type) " --border-label-pos=3 --color=label:italic:yellow --color=border:'#1D2021' \
-      --color=pointer:magenta --color=marker:green --header-border=line --footer-border=line | cut -d'|' -f3
-
-  )
-
-  bspc node $sela -g hidden=off -f
-  bspc node $sela -g sticky=off -f
-
-  if [ -f /tmp/external_m.txt ]; then
-    nw=$(bspc query -d 'DP-1-1:focused' -D --names)
-  else
-    nw=$(bspc query -D -d focused --names)
+    mq=$(bspc query -N -n .!hidden.window -d $cw)
+    type="mapped windows on the current desktop"
+  elif [ "$mode" = "vh" ]; then
+    mq=$(bspc query -N -n .hidden.window)
+    type="unmapped windows"
+  elif [ "$mode" = "va" ]; then
+    type="mapped windows on all desktops"
+    mq=$(bspc query -N -n .!hidden.window)
   fi
 
-  selw=$(sed -n "${nw}p" </tmp/randwall.txt)
-  hsetroot -cover $selw
+  if [ ! -f /tmp/widlist.txt ]; then
+    touch /tmp/widlist.txt
+  fi
 
+  echo -e "esc quit" >/tmp/widlist.txt
+
+  for wid in $mq; do
+
+    echo -e "$(xprop -notype -id $wid WM_NAME | cut -d'"' -f2) | $(xprop -notype -id $wid WM_CLASS | cut -d'"' -f2)\e[1;90m | $wid\e[0m" >>/tmp/widlist.txt
+
+  done
+
+  sel=$(
+
+    cat /tmp/widlist.txt | fzf --ansi --bind "ctrl-s:execute(appcommander.sh u)+become(appcommander.sh)" \
+      --info=hidden --header-lines=1 --border --padding=5%,0%,0%,0% \
+      --border-label=" Navigator ($type) " --border-label-pos=3 --color=label:italic:yellow --color=border:'#1D2021' \
+      --color=pointer:magenta --color=marker:green --header-border=line --footer-border=line | cut -d'|' -f3
+
+  )
+
+  #if [ $n = 0 ]; then
+  #  echo "Found no windows hidden by user"
+  #  sleep 1
+  #  exit
+  #fi
+
+  bspc node $sel -g hidden=off -f
+  bspc node $sel -g sticky=off -f
+
+  if [ "$mode" = "va" ]; then
+
+    if [ -f /tmp/external_m.txt ]; then
+      nw=$(bspc query -d 'DP-1-1:focused' -D --names)
+    else
+      nw=$(bspc query -D -d focused --names)
+    fi
+
+    selw=$(sed -n "${nw}p" </tmp/randwall.txt)
+    hsetroot -cover $selw
+  fi
+
+  exit 0
 }
 
 case $1 in
 -d)
-  desktop
+  mode="vd"
+  main
   ;;
 -h)
-  hidden
+  mode="vh"
+  main
   ;;
 -a)
-  all
+  mode="va"
+  main
   ;;
 *)
   exit 0
