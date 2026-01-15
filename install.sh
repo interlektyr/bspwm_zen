86aa78790ebf12815bf9f8af2ffb0d02d627d33d
interlektyr <interlektyr@hotmail.com>
2026-01-15 21:50:21 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -99,7 +99,7 @@ if [ "$ComT" = "ASUS" ]; then
 fi
 
 # install from the aur
-paru -S onlyoffice-bin conky-lua-nv marktext autojump moonbit tdrop-git rxvt-unicode-truecolor-wide-glyphs lemonbar-xft-git tomb tufw-bin bluetuith tremc mullvad-vpn-bin zen-browser-bin librewolf-bin aurutils
+paru -S dragon-drop onlyoffice-bin conky-lua-nv marktext autojump moonbit tdrop-git rxvt-unicode-truecolor-wide-glyphs lemonbar-xft-git tomb tufw-bin bluetuith tremc mullvad-vpn-bin zen-browser-bin librewolf-bin aurutils
 
 # check if /$HOME/.config/ exit, if it dosen't create it
 confdir="~/.config/"
